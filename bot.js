81cbeeb152366ca3bd3ba7cb902baa1a36324eb7
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2021-10-07 18:46:23 +0530

    :octocat: v1.2 Add functionality
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- bot.js
+++ bot.js
@@ -15,19 +15,45 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and Running"));
 bot.command("price", (ctx) => {
     const txt = ctx.message.text;
     const message = txt.split(" ");
-    if (message.length < 3) {
-        ctx.reply("Invalid Format!")
-    } else {
+
+    if (message.length == 3) {
         ctx.reply("Please wait... Fetching data from server...")
         const crypto = message[1]
         const currency = message[2]
         fetchPrice(crypto, currency)
-            .then((res)=>{
+            .then((res) => {
                 const rate = res.rate;
                 const reply = `Current Listing for ${crypto} in ${currency} is:` + "\n"
-                            +  `1 ${crypto} = ${rate} ${currency}.`;
-                ctx.reply(reply);
+                    + `1 ${crypto} = ${rate} ${currency}.`;
+                bot.api.editMessageText(ctx.message.chat.id, ctx.message.message_id + 1, reply);
             });
+    } else if (message.length == 2) {
+        const crypto = message[1]
+        ctx.reply("You have provided only one currency!")
+        fetchPrice(crypto, "USD")
+            .then((res) => {
+                const rate = res.rate;
+                const reply = `Current Listing for ${crypto} in USD is:` + "\n"
+                    + `1 ${crypto} = ${rate} USD.`;
+                bot.api.editMessageText(ctx.message.chat.id, ctx.message.message_id + 1, reply);
+            })
+        // bot.api.editMessageText(ctx.message.chat.id, ctx.message.message_id+1, "Changed Text")
+    } else if (message.length == 1) {
+        console.log(message.length)
+        ctx.reply("You are requesting the top 7 coins in the past 24hours.")
+        coinGeckoApi()
+            .then((res) => {
+                let coins = res.coins;
+                let topCoins = "CRYPTO in BTC";
+                // console.log(coins)
+                for (let coin of coins) {
+                    // console.log(coin.item.name);
+                    // topCoins.concat("\n"+coin.item.name + "\t" + Number.parseFloat(coin.item.price_btc).toPrecision(4))
+                    topCoins =topCoins.concat("\n", coin.item.name + "\t" + Number.parseFloat(coin.item.price_btc).toPrecision(4))
+                }
+                console.log(topCoins)
+                bot.api.editMessageText(ctx.message.chat.id, ctx.message.message_id+1, topCoins)
+            })
     }
 });
 
@@ -41,5 +67,10 @@ async function fetchPrice(crypto, currency) {
     return response.data;
 }
 
+
+async function coinGeckoApi() {
+    const response = await axios.get("https://api.coingecko.com/api/v3/search/trending");
+    return response.data;
+}
 
 bot.start();
